5782f735ce8f526a0898ac2e5766b86a57471eb7
zakky9362 <fikrimuhammadzakky@gmail.com>
2024-12-10 21:26:51 -0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,2 +1,2 @@
 cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.8/SRBMiner-Multi-2-6-8-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-6-8-Linux.tar.gz && clear && cd SRBMiner-Multi-2-6-8
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RMhikcxxJvGCV89eKt9bdXb2omUimnpxYa --Worker Rdpzakky3 --password x --cpu-threads 0
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RMhikcxxJvGCV89eKt9bdXb2omUimnpxYa --Worker Rdpzakky2 --password x --cpu-threads 0
